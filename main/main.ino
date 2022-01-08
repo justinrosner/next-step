9234f8b32947f24da6cd3a3d92e41b64448d5bd4
Justin Rosner <justinrosner6@gmail.com>
2022-01-08 11:22:55 -0500

    Initial Talkie code

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,23 @@
+#include <Arduino.h>
+#include "Talkie.h"
+
 int trigPin = 7;
 int echoPin = 5;
 int trigPin2 = 4;
 int echoPin2 = 3;
 
 void setup() {
+  justin();
+  /*
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
 
   Serial.begin(9600);
+  */
 }
 
 void loop() { 
-  MainUltraSensor();
+  //MainUltraSensor();
 }
---
+++ main/speaker.ino
@@ -0,0 +1,43 @@
+// Talkie library
+// Copyright 2011 Peter Knight
+// This code is released under GPLv2 license.
+
+//
+// Welcome to the Talkie library examples.
+//
+// Talkie is a speech synthesiser that works from a fixed vocabulary.
+//
+// There are hundreds of words in the 'Vocabulary' examples.
+//
+
+/*
+ * Voice PWM output pins for different ATmegas:
+ *  ATmega328 (Uno and Nano): non inverted at pin 3, inverted at pin 11.
+ *  ATmega2560: non inverted at pin 6, inverted at pin 7.
+ *  ATmega32U4 (Leonardo): non inverted at pin 10, inverted at pin 9.
+ *  ATmega32U4 (CircuitPlaygound): only non inverted at pin 5.
+ *
+ *  As default both inverted and not inverted outputs are enabled to increase volume if speaker is attached between them.
+ *  Use Talkie Voice(true, false); if you only need not inverted pin or if you want to use SPI on ATmega328 which needs pin 11.
+ *
+ *  The outputs can drive headphones directly, or add a simple audio amplifier to drive a loudspeaker.
+ */
+#include "Vocab_US_Large.h"
+
+Talkie voice;
+
+void justin() {
+    voice.doNotUseInvertedOutput();
+#if defined(TEENSYDUINO)
+    pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH); //Enable Amplified PROP shield
+#endif
+    voice.say(sp2_DANGER);
+    voice.say(sp2_DANGER);
+    voice.say(sp2_RED);
+    voice.say(sp2_ALERT);
+    voice.say(sp2_MOTOR);
+    voice.say(sp2_IS);
+    voice.say(sp2_ON);
+    voice.say(sp2_FIRE);
+}
