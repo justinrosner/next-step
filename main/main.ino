ce1c8d601e25dbf74375bf81b48fb07913692d06
Daniel Noorduyn <noorduyd@mcmaster.ca>
2022-01-12 16:02:35 -0500

    talkie sentences for speaker

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,38 @@
+#include <LiquidCrystal.h>
+
+#include <Mouse.h>
+
 #include <Arduino.h>
 #include "Talkie.h"
+#include <RPLidar.h>
 
+#define RPLIDAR_MOTOR 3 // The PWM pin for control the speed of RPLIDAR's motor.
+// This pin should connected with the RPLIDAR's MOTOCTRL signal
+//////////////////////////////////////////////////////////////////////////////
+
+// You need to create an driver instance
+RPLidar lidar;
+
+int nsbit = 0;
 
 int trigPin = 7;
 int echoPin = 5;
 int trigPin2 = 4;
 int echoPin2 = 3;
 
+float minDistance = 100000;
+float angleAtMinDist = 0;
+
+
 void setup() {
+  lidar.begin(Serial);
+
+  // set pin modes
+  pinMode(RPLIDAR_MOTOR, OUTPUT);
 
 }
 
 void loop() { 
+  mic();
+  run_lidar();
 }
--- main/microphone.ino
+++ main/microphone.ino
@@ -1,3 +1,48 @@
-// This file contains the code that enables the microphone functionality of 
-// NextStep that enables the user to use the microphone and provide the system
-// with information
+
+// Threshold (sound)
+int mic_threshold = 500;
+// Input pin .. on analog side
+int pin_mic = 0;
+// Ticks needed to change the state of the system
+int ticks_needed = 100;
+
+int mic_value;
+int is_on = false;
+int ticks = ticks_needed;
+
+void mic()
+{
+  // Audio value
+  mic_value = analogRead(pin_mic);
+  Serial.println(mic_value, DEC);
+  
+  // Is the read value ok? + did we had X ticks of silence?
+  if (mic_value >= mic_threshold && ticks > ticks_needed)
+  {
+    // System on or off?
+    if (is_on == 0)
+    {
+      is_on = 1;
+      // Do ACTION 1
+    }
+    else
+    {
+      is_on = 0;
+      // Do ACTION 2
+    }
+    ticks = 1;
+  }
+  else
+  {
+    if (ticks > ticks_needed)
+    {
+      ticks = ticks_needed + 1;
+    }
+    else
+    {
+      ticks++;
+    }
+  }
+  //Serial.println(mic_value);
+  delay(10);
+}
