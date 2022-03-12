63806647fed53bc73fa827f3d87b4f1db0c544f2
nA-div-B-equ-kB-kER <tishko99a@gmail.com>
2022-03-12 15:41:12 -0500

    added compatblity with rest of ino files
    update function names as well as update array used bby bubble band

--- main/sensorFusion.ino
+++ main/sensorFusion.ino
@@ -22,13 +22,14 @@ void fusionSetup() {
 
 }
 
-float [] fusionLoop() {
+void MainSensorFusion() {
   // put your main code here, to run repeatedly:
+  int angle;
   for(int i=0; i<len-1; i++){
     dist[i]=s[i].sensorLoop();  
     //check if range intersects with lidar angle measured
     //take out the list of points it is not needed only need current one!
-    int angle=L.points[L.pointInd].angle;
+    angle=L.points[L.pointInd].angle;
     if(angle<=s[i].getCenter()+s[i].getRange()/2 && angle>=s[i].getCenter()-s[i].getRange()/2){
        dist[i]=fuseDist(L.distaway, s[i].getDistance()) ;
        dist[len]=dist[i];
@@ -44,12 +45,43 @@ float [] fusionLoop() {
   if(!intersect){
     //output distance of lidar as well and angle it was detecting at for bubble band! 
     dist[len-1]=L.sensorLoop();  
+    //check which bubble the lidar is in and which sensor within this rnge and update it as well as the global array
+    if(90>=angle && angle<54){
+      if(dist[len-1]<dist[0]){
+        fused_distances[0]=dist[len-1];
+      }
+      else{fused_distances[0]=dist[0];}
+    }
+    else if(54>=angle && angle<18){
+      if(dist[len-1]<dist[1]){
+        fused_distances[1]=dist[len-1];
+      }
+      else{fused_distances[1]=dist[1];}
+    }
+    else if(angle>=342 || angle<=18){
+      if(dist[len-1]<dist[2]){
+        fused_distances[2]=dist[len-1];
+      }
+      else{fused_distances[2]=dist[2];}
+    }
+    else if(angle<342 && angle>=308){
+      if(dist[len-1]<dist[3]){
+        fused_distances[3]=dist[len-1];
+      }
+      else{fused_distances[3]=dist[3];}
+    }
+    else{
+        if(dist[len-1]<dist[4]){
+        fused_distances[4]=dist[len-1];
+      }
+      else{fused_distances[4]=dist[4];}
+    }
   }
   else{
     L.updatePrevDist(dist[len]*10);
   }
   
-  return dist;
+  //return dist;
 }
 
 float fuseDist(float distLidar, float distUS){
