98652b86aa3b06ae17dfef290f27a52a48e26638
Mengxi (William) Lei <42485047+William-MLei@users.noreply.github.com>
2022-03-12 13:53:47 -0500

    Update main to Include System Diagnostic Module
    Updated main.ino to include and initialize the SystemDiagnostic class/object

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,8 @@
 // Main file
 
+// Import
+#include "system_diagnostic.h"
+
 // Global Declarations used across files
 #define N 5
 double distances[N];          // Ultrasonic distances
@@ -7,12 +10,13 @@ double fused_distances[N];    // fused distance points for each of the 5 sectors
 double velocity = 0.0;          // Velocity of user (assumed to be forwards)
 double time = millis();
 double time_since_last_reading = 0.0;
+SystemDiagnostic diagnosticModule;
 
 void setup() {
   Serial.begin(9600);
-  HapticMotorSetup();
   UltraSetup();
   LidarSetup();
+  diagnosticModule = *(new SystemDiagnostic());
 }
 
 void loop() {
