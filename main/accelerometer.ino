b83d2d8c1628e070cb6f42ef8e4b4222d03067a0
nA-div-B-equ-kB-kER <tishko99a@gmail.com>
2022-03-29 01:51:34 -0400

    added offset and filtering
    adding offest finding code in the setup and the filtering of acceleration in the getXAcceleration function call

--- main/accelerometer.ino
+++ main/accelerometer.ino
@@ -1,6 +1,6 @@
 // This file contains all of the code relating to gathering velocity data from
 // the accelerometer
-
+#include "Kalman.h"
 // Constants for moving point average
 #define WINDOW_SIZE 20
 
@@ -10,7 +10,8 @@ struct AccelInformation {
   double sum = 0;
   double reading[WINDOW_SIZE];
 };
-
+float velocity=0;
+Kalman filt(10, 0.00001);
 // Pin Assignments, these need to be assigned to analog pins
 const int x_pin = A0;
 const int y_pin = A1;
@@ -19,32 +20,48 @@ const int z_pin = A2;
 // Constants for accelrometer readings
 // TO-DO: Need to adjust the accel_offset number once we mount the accelerometer
 // into its final position
-const double accel_offset = 355.0; 
+double accel_offset = 0; 
 const double prev_accel = 0;
+double acceleration;
 
 // Create an array of SensorInformation structs
 struct AccelInformation accelerometer_readings;
 
 // This is a helper function to get the acceleration in the x direction
 double getXAcceleration() {
-    double acceleration = accel_offset - analogRead(x_pin);
-    double filtered_acceleration = MovingPointAverage(accelerometer_readings.sum,
+    acceleration = accel_offset - analogRead(x_pin);
+    /*double filtered_acceleration = MovingPointAverage(accelerometer_readings.sum,
                                                accelerometer_readings.reading,
                                                accelerometer_readings.index,
-                                               acceleration);
-    Serial.println("X Accel: " + String(filtered_acceleration) + " ");
-    bool possibleError = diagnosticModule.checkAccelerometer(filtered_acceleration, velocity);
+                                               acceleration);*/
+    double filtered_acceleration = filt.nextEstimate(acceleration);
+    //Serial.println("X Accel: " + String(filtered_acceleration) + " ");
     return filtered_acceleration;
 }
 
 // This function updates the velocity global variable and returns the
 // new updated value
+float time1;
+float time_since_last_reading;
 void UpdateVelocity() {
     // Calculate the time since the last time reading
     float cur_time = millis();
-    time_since_last_reading = (cur_time - time);
-    time = time_since_last_reading;
-    double accel = prev_accel + getXAcceleration();
+    time_since_last_reading = (cur_time - time1);
+    time1 = time_since_last_reading;
+    double accel = 0 + getXAcceleration();
     velocity += accel * (time_since_last_reading / 1000);
-    Serial.println("Velocity: " + String(velocity));
+    //Serial.println("Velocity: " + String(velocity));
+}
+
+void accleSetup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  getXAcceleration();
+  accel_offset= -1*acceleration;
+
+}
+
+void AccelLoop() {
+  //getXAcceleration();
+  UpdateVelocity();
 }
